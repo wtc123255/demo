c7326b1e31597cdabca83bb029f5f1974205d301
SAMSUNG <SAMSUNG@LAPTOP-T7NQPIGB>
2019-03-26 22:56:04 +0800

    测试

--- src/com/wangtiancheng/demo/Test01.java
+++ src/com/wangtiancheng/demo/Test01.java
@@ -3,6 +3,7 @@ package com.wangtiancheng.demo;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("王天诚");
+		System.out.println("王天诚");		
+		System.out.println("王天诚");		
 	}
 }
