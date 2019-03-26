3355e545670691421bc6e5155ef7c135557d0f20
lifeng <alifenga@163.com>
2019-03-26 23:13:10 +0800

    作业01

---
+++ src/com/wangtiancheng/demo/Work01.java
@@ -0,0 +1,20 @@
+package com.wangtiancheng.demo;
+
+/** 
+ * @CompanyName lhfinance.com
+ * @ClassName Work01.java 
+ * @Package com.wangtiancheng.demo 
+ * @Description: (作业01) 
+ * @author lf  
+ * @date 2019年3月26日 下午11:09:18 
+ * @version V1.0.0
+ *
+  * 题目:用数组接收用户输入的10个值
+  * 去掉最大数和最小数打印剩余的8个值,和打印从小到大排序的8个值
+  * 打印这8个值的和以及平均数
+ */
+public class Work01 {
+	public static void main(String[] args) {
+		
+	}
+}
