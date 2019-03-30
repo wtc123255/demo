64ce1392a04cd0eba7f831c5a68c0ed1b256368a
lifeng <alifenga@163.com>
2019-03-30 18:08:50 +0800

    add work08

---
+++ src/com/wangtiancheng/work/Work09.java
@@ -0,0 +1,49 @@
+package com.wangtiancheng.work;
+
+/** 
+  * @Email alifenga@163.com
+  * @ClassName Work09.java 
+  * @Package com.wangtiancheng.work 
+  * @Description: (用一句话描述该文件做什么) 
+  * @author lf  
+  * @date 2019年3月30日 下午6:00:04 
+  * @version V1.0.0
+  *
+  * 假如有个人叫老王,他有100万的遗产,有一天他死了,他儿子小王继承了100万遗产,
+  * 后来他工作赚了20万,娶老婆用了40万,剩余80万
+  * 用继承实现小王是如何继承老王的遗产并消费的,手动帮小王调用赚钱和娶老婆的方法
+  * 打印小王继承遗产时候的金额,和继承使用后的金额
+  */
+public class Work09 {
+	public static void main(String[] args) {
+	}
+}
+
+/**
+ * 老王
+  * @Email alifenga@163.com
+  * @ClassName Work09.java 
+  * @Package com.wangtiancheng.work 
+  * @Description: (用一句话描述该文件做什么) 
+  * @author lf  
+  * @date 2019年3月30日 下午6:03:02 
+  * @version V1.0.0
+  *
+ */
+class Lw{
+}
+
+/**
+ * 小王
+  * @Email alifenga@163.com
+  * @ClassName Work09.java 
+  * @Package com.wangtiancheng.work 
+  * @Description: (用一句话描述该文件做什么) 
+  * @author lf  
+  * @date 2019年3月30日 下午6:03:21 
+  * @version V1.0.0
+  *
+ */
+class Xw extends Lw{
+}
+
