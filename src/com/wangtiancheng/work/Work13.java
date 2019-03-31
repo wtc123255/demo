5224fa2329943dd14e2c429839d16ecb381e9d5e
lifeng <alifenga@163.com>
2019-03-31 16:07:25 +0800

    添加思路

--- src/com/wangtiancheng/work/Work13.java
+++ src/com/wangtiancheng/work/Work13.java
@@ -3,6 +3,12 @@ package com.wangtiancheng.work;
 /**
  * 	输入一个字符判断是否回文串  如abcdcba,12321 上海自来水来自海上  是回文串 正反读都是一样
  * 
+ * 
+ *  最简单实现思路:
+ *  StringBuffer sb = new StringBuffer(str);  
+ *  sb.reverse();// 将Str中的字符串倒置 
+ *  字符串倒置后去比较原字符串内容是否相同
+ * 
  */
 public class Work13 {
 	public static void main(String[] args) {
