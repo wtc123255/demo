9bbf26d639e188bea7f546aa77dbf2efd9b12b5d
wangtiancheng <1131195621@qq.com>
2019-03-31 20:21:23 +0800

    Work

--- src/com/wangtiancheng/work/Work12.java
+++ src/com/wangtiancheng/work/Work12.java
@@ -9,14 +9,14 @@ public class Work12 {
 	public static void main(String[] args) {
 		int i = 0;
         int j = 0;
-        int k = 0;
+        int l = 0;
         int b = 0;
         for(i = 1; i <= 4; i++) {
             for(j = 1; j <= 4; j++) {
-                for(k = 1; k <= 4; k++) {
-                    if(i != j&& j != k && i!= k) {
+                for(l = 1; l <= 4; l++) {
+                    if(i != j&& j != l && i!= l) {
                         b += 1;
-                        System.out.println(i*100 + j*10 + k);
+                        System.out.println(i*100 + j*10 + l);
                     }
                 }
             }
